7c00b15c9bd617ddaffa4ba3b063e9f89e419061
AliReza Heidari <h118ar@gmail.com>
2023-04-28 15:04:07 +0400

    map ssg

--- pages/Map/index.js
+++ pages/Map/index.js
@@ -2,65 +2,46 @@ import Map from "../../components/Map/Map"
 import SecMain from "../../components/Map/secMain"
 import Select from "../../components/Map/Select/Select"
 import CardSubscribe from "../homePage/secCardSubscribe/CardSubscribe"
-import { useEffect, useState } from "react"
-import axios from "axios"
+import { useState } from "react"
 
-const MapPage = () => {
 
-  const [data,setData] = useState({data:{},loading:false})
-  const [floorModal,SetFloorModal] = useState(false)
-  const [changeFloor,SetChangeFloor] = useState(1)
-
-  const GetMapData = async() => {
-    try {
-        const response = await axios({
-            url: "https://apimall.weflex.am/api/map",
-            method: "GET"
-        })
-        // console.log(response,"mapData");
-        if(response.data && response.status == 200){
-            setData({
-                data:response.data,
-                loading:true
-            })
-        }
-    } catch (error) {
-        console.log(error);
+const MapPage = ({data}) => {
+    const [floorModal, SetFloorModal] = useState(false)
+    const [changeFloor, SetChangeFloor] = useState(1)
+    const setFloor = (num) => {
+        SetChangeFloor(num == changeFloor ? num : num)
+        SetFloorModal(false)
     }
-  }
-    useEffect(() => {
-      GetMapData()
-    },[])
 
-  const setFloor = (num) => {
-      SetChangeFloor(num == changeFloor ? num : num)
-      SetFloorModal(false)
-  }
-  
 
-    return(
+    return (
         <div>
-            <SecMain/>
-            <Select data={data} setFloor={setFloor} changeFloor={changeFloor} floorModal={floorModal} SetFloorModal={SetFloorModal}/>
-            <Map  data={data} changeFloor={changeFloor}/>
-            <CardSubscribe/>
+            <SecMain />
+            <Select data={data} setFloor={setFloor} changeFloor={changeFloor} floorModal={floorModal} SetFloorModal={SetFloorModal} />
+            <Map data={data} changeFloor={changeFloor} />
+            <CardSubscribe />
         </div>
     )
 }
 
-// export async function getStaticProps() {
-    
-    
-//          const res = await fetch("https://apimall.weflex.am/api/map")
-//          const data = await res.json()
+export async function getStaticProps() {
+
+
+    const mapFetch = await fetch("https://apimall.weflex.am/api/map")
+    const map = await mapFetch.json()
+    console.log('map: ', map);
     
 
-//      return {
-//        props: {
-//          data,
-//        },
-//      }
-//    }
+
+    return {
+        props: {
+            data: {
+                data: map,
+                loading: true
+            }
+        },
+    }
+}
 
 
 
