c00465b17c87f325c2eaa1d3c74d08eb3341cc34
AliReza Heidari <h118ar@gmail.com>
2023-04-28 23:56:00 +0400

    add meta title

--- components/layout.js
+++ components/layout.js
@@ -15,7 +15,7 @@ const Layout = ({ children }) => {
                 <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=5' />
                 <meta name='description' content='Description' />
                 <meta name='keywords' content='Keywords' />
-
+                <title>Magic Mall</title>
             </Head>
 
             <Header />
