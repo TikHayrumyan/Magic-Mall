5bad2145230de950c1cfbf34701a181e856caf5f
AliReza Heidari <h118ar@gmail.com>
2023-04-28 22:52:35 +0400

    with error img

--- next.config.js
+++ next.config.js
@@ -3,57 +3,58 @@
 const webpack = require("webpack");
 
 const nextConfig = {
-  
-  reactStrictMode: true,
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.plugins.push(
-      new webpack.ProvidePlugin({
-        $: "jquery",
-        jQuery: "jquery",
-        "window.jQuery": "jquery",
-      })
-    )
-    return config;
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "**",
+            },
+            {
+                protocol: 'https',
+                hostname: '**.weflex.am',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'http',
+                hostname: 'apimall.weflex.am',
+                port: '',
+                pathname: '/images/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'apimall.weflex.am',
+                port: '',
+                pathname: '/images/**',
+            },
+            {
+                protocol: 'http',
+                hostname: 'weflex.am',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
 
-  i18n: {
+    reactStrictMode: true,
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        config.plugins.push(
+            new webpack.ProvidePlugin({
+                $: "jquery",
+                jQuery: "jquery",
+                "window.jQuery": "jquery",
+            })
+        )
+        return config;
+    },
 
-    locales: ['en', 'am', 'ru'],
-    defaultLocale: 'am',
-    localeDetection: false
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-      {
-        protocol: 'https',
-        hostname: '**.weflex.am',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'http',
-        hostname: 'apimall.weflex.am',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'apimall.weflex.am',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'http',
-        hostname: 'weflex.am',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+    i18n: {
+
+        locales: ['en', 'am', 'ru'],
+        defaultLocale: 'am',
+        localeDetection: false
+    },
+
 
 
 }
